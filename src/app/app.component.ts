fdcf3d270cf278101679aed7206091843e58c5fa
Tux Penguin <tuxpengwin@gmail.com>
2021-10-27 15:15:58 -0700

    updaet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { FirstService } from './services/first.service';
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { ChatMessage } from './models/chat';
+import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
+	selector: 'base-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-	isLoaded: boolean = false;
-
-	data: ChatMessage[] = [];
-
-	constructor(public service: FirstService) { }
-
-	ngOnInit(): void {
-		this.service.getMessage().subscribe((data) => {
-			this.data = data;
-		});
-	}
-
-
-	toggleLoaded() {
-		this.isLoaded = !this.isLoaded;
-	}
+export class AppComponent {
+	title = 'pengaelusmessengerclient';
 }
