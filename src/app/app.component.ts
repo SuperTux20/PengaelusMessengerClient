ef649a118cea2d7d86be4966c7815be7b0d64372
Tux Penguin <tuxpengwin@gmail.com>
2021-12-02 15:08:53 -0800

    refactor output-setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 @Component({
-	selector: 'base-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+	selector: "base-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
 	constructor(private http: HttpClient) { }
@@ -19,10 +19,11 @@ export class AppComponent {
 	selected: number = 0;
 
 	switchMenu(val: string) { this.selected = parseInt(this.actions.indexOf(val).toString()); }
+	setOutput(res: Object) { document.getElementById("output")!.innerText = JSON.stringify(res, null, "|   "); }
 
-	gac() { this.http.get(this.url).subscribe(res => document.getElementById("output")!.innerText = JSON.stringify(res)) }
-	gamic() { this.http.get(this.url + document.getElementsByTagName("input")[0].value).subscribe(res => document.getElementById("output")!.innerText = JSON.stringify(res)) }
-	snmic() { this.http.post(this.url + document.getElementsByTagName("input")[1].value, { username: document.getElementsByTagName("input")[1].value, message: document.getElementsByTagName("input")[0].value }).subscribe(res => document.getElementById("output")!.innerText = JSON.stringify(res)) }
-	uc() { this.http.patch(this.url + document.getElementsByTagName("input")[0].value, { name: "general" }).subscribe(res => document.getElementById("output")!.innerText = JSON.stringify(res)) }
-	dc() { this.http.delete(this.url + document.getElementsByTagName("input")[0].value).subscribe(res => document.getElementById("output")!.innerText = JSON.stringify(res)) }
+	gac() { this.http.get(this.url).subscribe(res => this.setOutput(res)) }
+	gamic() { this.http.get(this.url + document.getElementsByTagName("input")[0].value).subscribe(res => this.setOutput(res)) }
+	snmic() { this.http.post(this.url + document.getElementsByTagName("input")[1].value, { username: document.getElementsByTagName("input")[1].value, message: document.getElementsByTagName("input")[0].value }).subscribe(res => this.setOutput(res)) }
+	uc() { this.http.patch(this.url + document.getElementsByTagName("input")[0].value, { name: "general" }).subscribe(res => this.setOutput(res)) }
+	dc() { this.http.delete(this.url + document.getElementsByTagName("input")[0].value).subscribe(res => this.setOutput(res)) }
 }
